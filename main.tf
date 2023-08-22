3391bfb058d3fbd6b8557926902ef8c67ff3c894
hemant96c <132544701+hemant96c@users.noreply.github.com>
2023-08-22 11:45:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = data.aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "My_Private_Subnet"
   }
 }
 
-provider "aws" {
-  region = "ap-south-1"
-
-}
 
 
-# Create routing table
 resource "aws_route_table" "my_route_table" {
   vpc_id = data.aws_vpc.vpc.id
 
@@ -27,44 +22,17 @@ resource "aws_route_table" "my_route_table" {
   }
 }
 
-# # Create routing table
-# resource "aws_route_table" "my_route_table1" {
-#   vpc_id = data.aws_vpc.vpc.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = data.aws_nat_gateway.nat.id
-#   }
 
-#   tags = {
-#     Name = "My Route Table"
-#   }
-# }
-
-# Associate subnet with the routing table
 resource "aws_route_table_association" "my_route_table_association" {
-  subnet_id = data.aws_subnet.subnet.id
+  subnet_id = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.my_route_table.id
 }
 
-# # Create a route in the private route table for internet-bound traffic
-# resource "aws_route" "private_route" {
-#   route_table_id         = aws_route_table.my_route_table1.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   nat_gateway_id         = data.aws_nat_gateway.nat.id
-# }
 
-# Create security group
 resource "aws_security_group" "my_security_group" {
   name_prefix = "My Security Group"
   vpc_id      = data.aws_vpc.vpc.id
-
-  # ingress {
-  #   from_port   = 0
-  #   to_port     = 65535
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["${data.aws_vpc.vpc.cidr_block}"]
-  # }
 
   ingress {
     from_port   = 0
@@ -82,9 +50,6 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-
-
-# Create Lambda function
 resource "aws_lambda_function" "my_lambda_function" {
   filename         = "lambda.zip"
   function_name    = "lambda"
@@ -94,49 +59,12 @@ resource "aws_lambda_function" "my_lambda_function" {
   timeout = 63
   vpc_config {
     security_group_ids = [aws_security_group.my_security_group.id]
-    subnet_ids         = [data.aws_subnet.subnet.id]
+    subnet_ids         = [aws_subnet.private_subnet.id]
   }
 
   environment {
     variables = {
-      subnet = "${data.aws_subnet.subnet.id}"
+      subnet = "${aws_subnet.private_subnet.id}"
     }
   }
-}
-
-resource "aws_iam_policy" "lambda-policy" {
-  name = "lambda-ec2-stop-start"
-
-  policy = jsonencode(
-    {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "LambdaVPCAccess",
-      "Effect": "Allow",
-      "Action": [
-        "ec2:CreateNetworkInterface",
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:DeleteNetworkInterface",
-        "ec2:AssignPrivateIpAddresses",
-        "ec2:UnassignPrivateIpAddresses",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeVpcs",
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-
-
-  )
-}
-
-resource "aws_iam_role_policy_attachment" "lambda-ec2-policy-attach" {
-  policy_arn = aws_iam_policy.lambda-policy.arn
-  role = data.aws_iam_role.lambda.name
 }
