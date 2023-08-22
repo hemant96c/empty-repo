3c80ed4681bee3d2c28b8f371e01281649dfd5af
hemant96c <132544701+hemant96c@users.noreply.github.com>
2023-08-22 13:49:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,9 +42,9 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-resource "aws_lambda_function" "my_lambda_function" {
+resource "aws_lambda_function" "my_lambda_function1" {
   filename         = "lambda.zip"
-  function_name    = "lambda"
+  function_name    = "lambda1"
   role             = data.aws_iam_role.lambda.arn
   handler          = "lambda.lambda_handler"
   runtime          = "python3.7"
