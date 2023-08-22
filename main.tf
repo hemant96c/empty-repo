2bb227f5e8b9dd05affc4e76a25831f8c9e61461
hemant96c <132544701+hemant96c@users.noreply.github.com>
2023-08-22 12:32:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "my_security_group" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
